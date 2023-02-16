bf63fcec284edb57d1e6253caa1fe70c2d1e025f
Nizar Selander <nizar.selander@gmail.com>
2023-02-16 11:40:10 +0100

    chore: grouped scoring stratigies into their own directory
    Moved scoring strategy interface and implementing classes to their own directory to make it easier to tell what is what.

--- Bowling/prog.cs
+++ Bowling/prog.cs
@@ -7,25 +7,25 @@ class GFG {
     // Main Method
     public static void Main()
     {
-        // Series 1
+        // Series 1: Basic Scoring
         var series1 = new Game("Series1.txt");
         var series1Winner = series1.GetWinner();
         Console.WriteLine("Series 1 winner is:");
         Console.WriteLine("{0} ({1} points).", series1Winner.Name, series1Winner.Score);
         
-        // Series 2
+        // Series 2: Spares and Strikes Rewarded
         var series2 = new Game("Series2.txt", new SparesAndStrikesScoring());
         var series2Winner = series2.GetWinner();
         Console.WriteLine("Series 2 winner is:");
         Console.WriteLine("{0} ({1} points).", series2Winner.Name, series2Winner.Score);
         
-        // Series 3
+        // Series 3: Spares and Streaks Rewarded with streak-based bonuses
         var series3 = new Game("Series3.txt", new StreaksScoring());
         var series3Winner = series3.GetWinner();
         Console.WriteLine("Series 3 winner is:");
         Console.WriteLine("{0} ({1} points).", series3Winner.Name, series3Winner.Score);
         
-        // Series 4
+        // Series 4: Just get weird with it
         var series4 = new Game("Series4.txt", new ElectricBoogalooScoring());
         var series4Winner = series4.GetWinner();
         Console.WriteLine("Series 4 winner is:");
